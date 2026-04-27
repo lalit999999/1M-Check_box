d47d37a427d74e16f7eb34e999faff1e231e9e54
Lalit gurjar <gujarlalit79@gmail.com>
2026-04-27 17:37:38 +0530

    realtime changes in checkbox is running now.

--- index.js
+++ index.js
@@ -2,59 +2,26 @@ import http from 'node:http';
 import path from 'node:path';
 import express from 'express';
 import { Server } from 'socket.io';
-import { connectRedis, subscribeToCheckboxChanges, publishCheckboxChange, redis } from './redis-connection.js';
 
-const CHECKBOX_COUNT = 100;
-const state = {
-    checkboxes: new Array(CHECKBOX_COUNT).fill(false)
-};
 
 async function main() {
-    // Connect to Redis
-    await connectRedis();
 
     const app = express();
     const server = http.createServer(app);
+
+
     const io = new Server();
     io.attach(server);
 
-    // Subscribe to Redis pub/sub for checkbox changes from other servers
-    await subscribeToCheckboxChanges((data) => {
-        console.log(`[Redis]:checkbox:change`, data);
-
-        // Update the in-memory state
-        state.checkboxes[data.index] = data.checked;
-
-        // Broadcast to all clients on this server
-        io.emit('server:checkbox:change', data);
-    });
-
-    // Socket IO Handler
     io.on('connection', (socket) => {
         console.log(`Socket connected`, { id: socket.id });
 
-        // Send initial state to newly connected client
-        socket.emit('server:checkbox:initial', state.checkboxes);
-
-        socket.on('client:checkbox:change', async (data) => {
-            console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
-            // take existing state from reddis and update it with the new change, then publish to redis and broadcast to all clients
-            const existingState = await redis.get('CHECKBOX_STATE');
-
-            // Update the in-memory state
-            state.checkboxes[data.index] = data.checked;
-
-            // Publish to Redis so other servers get the update
-            await publishCheckboxChange(data);
-
-            // Broadcast to all connected clients on this server
-            io.emit('server:checkbox:change', data);
-        });
-
-        socket.on('disconnect', () => {
-            console.log(`Socket disconnected`, { id: socket.id });
+        socket.on('client:checkbox:change', (data) => {
+            console.log(`Received checkbox change from client ${socket.id}:`, data);
+            io.emit('server:checkbox:update', { id: socket.id, ...data });
         });
     });
+
 
     // Express handlers
     app.use(express.static(path.resolve('./public')));
@@ -63,11 +30,8 @@ async function main() {
         res.json({ healthy: true });
     });
 
-    app.get('/checkboxes', (req, res) => {
-        res.json({ checkboxes: state.checkboxes });
-    });
 
-    const port = process.env.PORT ?? 8000;
+    const port = process.env.PORT ?? 3300;
 
     server.on('error', (err) => {
         if (err.code === 'EADDRINUSE') {
